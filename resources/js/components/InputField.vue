15854e4fb4c6a45463fc3ce756aee2df3188e893
Aryeh Raber <aryeh.raber@gmail.com>
2023-05-31 18:50:15 +0200

    Update components for Statamic 4

--- resources/js/components/InputField.vue
+++ resources/js/components/InputField.vue
@@ -6,21 +6,22 @@
   </div>
 
   <div class="asset-solo-container asset-grid-listing" v-else>
-    <div :title="value.description" class="asset-tile col-span-2 is-image" tabindex="0">
+    <div :title="value.description" class="asset-tile is-image" tabindex="0">
       <div class="asset-thumb-container">
         <div class="asset-thumb">
           <img :src="value.urls.small" :alt="value.description" :title="value.description">
 
           <div class="asset-controls">
-            <button class="btn btn-icon" @click="$emit('remove')">
-              <svg-icon name="trash" class="w-4 h-4"/>
+            <button class="btn btn-icon" @click="$emit('remove')" :alt="__('Remove')">
+              <svg-icon name="micro/sharp-trash" class="h-4 my-2"/>
             </button>
           </div>
         </div>
       </div>
+
       <div class="asset-meta">
         <div
-          class="asset-filename"
+          class="asset-filename flex-1 px-2 py-1"
           :title="value.description"
           v-text="value.description"
         />
