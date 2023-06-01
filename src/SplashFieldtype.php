84151b01887cde62d973bf00c5232614566ba38c
Aryeh Raber <aryeh.raber@gmail.com>
2023-06-01 14:21:26 +0200

    Reduce default data saved

--- src/SplashFieldtype.php
+++ src/SplashFieldtype.php
@@ -2,6 +2,7 @@
 
 namespace AryehRaber\Splash;
 
+use Illuminate\Support\Arr;
 use Statamic\Fields\Fieldtype;
 
 class SplashFieldtype extends Fieldtype
@@ -13,5 +14,24 @@ class SplashFieldtype extends Fieldtype
     public function preload()
     {
         return config('splash', []);
+    }
+
+    public function process($data)
+    {
+        if (is_null($data)) {
+            return $data;
+        }
+
+        $default = [
+            'description',
+            'urls.raw',
+            'user.name',
+            'user.links.html',
+        ];
+
+        return collect($default)
+            ->mapWithKeys(fn ($key) => [$key => Arr::get($data, $key)])
+            ->undot()
+            ->all();
     }
 }
